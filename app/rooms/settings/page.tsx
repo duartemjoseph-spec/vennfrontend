f1bd8597c0a38363f1a63fd9005f8704f4e6b04b
dherrera-code <dherr929@gmail.com>
2026-04-16 16:16:10 -0700

    add buttons in privacy and security settings!

--- app/rooms/settings/page.tsx
+++ app/rooms/settings/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Container from '@/components/Container'
-import { ToggleSwitch } from 'flowbite-react'
-import { Bell, Globe, Moon, Shield } from 'lucide-react'
+import { Button, ToggleSwitch } from 'flowbite-react'
+import { Bell, CircleQuestionMark, Globe, Lock, Moon, Shield } from 'lucide-react'
 import React, { useState } from 'react'
 
 const page = () => {
@@ -34,7 +34,7 @@ const page = () => {
             {/* insert settings for notifications, email notifications, sound effects! */}
             <div className='flex justify-between'>
               <div>
-                <h2 className='text-xl text-zinc-900 font-semibold'>Push Notifications</h2>
+                <h2 className='text-lg text-zinc-900 font-semibold'>Push Notifications</h2>
                 <p className='text-zinc-500'>Receive push notifications on this device</p>
               </div>
               <div className='flex items-center'>
@@ -46,7 +46,7 @@ const page = () => {
 
             <div className='flex justify-between'>
               <div>
-                <h2 className='text-xl text-zinc-900 font-semibold'>Email Notifications</h2>
+                <h2 className='text-lg text-zinc-900 font-semibold'>Email Notifications</h2>
                 <p className='text-zinc-500'>Receive updates via email</p>
               </div>
               <div className='flex items-center'>
@@ -54,11 +54,12 @@ const page = () => {
                 <ToggleSwitch color='dark' checked={true} onChange={handleNotification} ></ToggleSwitch>
               </div>
             </div>
+
             <hr className='pt-5 mt-5' />
 
             <div className='flex justify-between'>
               <div>
-                <h2 className='text-xl text-zinc-900 font-semibold'>Sound Effects</h2>
+                <h2 className='text-lg text-zinc-900 font-semibold'>Sound Effects</h2>
                 <p className='text-zinc-500'>Play sounds for notifications and actions</p>
               </div>
               <div className='flex items-center'>
@@ -73,7 +74,7 @@ const page = () => {
         <div className='mt-5 rounded-xl border border-zinc-200 bg-white p-5 shadow-sm '>
           <div className='flex gap-4 items-center'>
             <div className='gap-2 rounded-xl place-items-center bg-blue-100 p-3 w-11 h-11 transition '>
-              
+
               <Moon aria-label='Settings to toggle dark mode!' color='#2F80ED' size={22}></Moon>
             </div>
             <div>
@@ -82,6 +83,26 @@ const page = () => {
             </div>
           </div>
 
+          <div className='pt-8 pb-2'>
+
+            <div className='flex justify-between'>
+              <div>
+                <h2 className='text-lg text-zinc-900 font-semibold'>Dark Mode</h2>
+                <p className='text-zinc-500'>Switch to dark theme</p>
+              </div>
+              <div className='flex items-center'>
+                {/* radio button */}
+                <ToggleSwitch color='dark' checked={boolNotification} onChange={handleNotification} ></ToggleSwitch>
+              </div>
+            </div>
+
+          </div>
+          <hr className='pt-5 mt-5' />
+
+          <div>
+            <h2 className='text-lg text-zinc-900 font-semibold'>Language</h2>
+            {/* insert toggle  */}
+          </div>
 
         </div>
 
@@ -89,7 +110,7 @@ const page = () => {
         <div className='mt-5 rounded-xl border border-zinc-200 bg-white p-5 shadow-sm '>
           <div className='flex gap-4 items-center'>
             <div className='gap-2 rounded-xl place-items-center bg-sky-100 p-3 w-11 h-11 transition '>
-              
+
               {/* <Moon aria-label='Settings to toggle dark mode!' color='#2F80ED' size={22}></Moon> */}
               <Globe size={21} color='#2F80ED'></Globe>
             </div>
@@ -98,7 +119,13 @@ const page = () => {
               <p className='text-zinc-500'>Configure timezone and regional settings</p>
             </div>
           </div>
+        <hr className='pt-5 mt-5' />
 
+          <div>
+            <h2 className='text-lg text-zinc-900 font-semibold'>Timezone</h2>
+            {/* insert toggle  */}
+
+          </div>
 
         </div>
         {/* Privacy & Security */}
@@ -114,7 +141,23 @@ const page = () => {
               <p className='text-zinc-500'>Manage your privacy and security settings</p>
             </div>
           </div>
+          <hr className='pt-5 mt-5' />
 
+          <div className='flex flex-col gap-5'>
+            <div className='p-4 py-3 border shadow-black rounded-lg hover:bg-gray-300 flex items-center gap-3'>
+              <Lock size={20} color='black' />
+              <h2 className='text-gray-800 font-semibold'>Change Password</h2>
+            </div>
+            <div className='p-4 py-3 border shadow-black rounded-lg hover:bg-gray-300 flex items-center gap-3'>
+              <Shield size={20} color='black' />
+              <h2 className='text-gray-800 font-semibold'>Privacy Settings</h2>
+            </div>
+            <div className='p-4 py-3 border shadow-black rounded-lg hover:bg-gray-300 flex items-center gap-3'>
+              <CircleQuestionMark size={20} color='black' />
+              <h2 className='text-gray-800 font-semibold'>Data & Privacy</h2>
+            </div>
+            {/* insert toggle  */}
+          </div>
 
         </div>
 

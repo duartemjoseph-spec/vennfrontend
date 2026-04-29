f51237fe6dcfdae20d285b032fc5b26da6a88e4d
dherrera-code <dherr929@gmail.com>
2026-04-29 10:09:50 -0700

    fixed radio buttons in settings page and removed dark mode from rendering by flowbite-react

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeInit } from "../.flowbite-react/init";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeInit />
         {children}
       </body>
     </html>
